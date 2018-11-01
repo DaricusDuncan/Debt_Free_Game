e4b4e37f3b9dedd1f4a24327e2dc1f8689320d9c
Lando Shepherd <landoshepherd@landos-macbook-pro.local>
2018-10-31 23:56:07 -0500

    Changed the utilities class to DFGValidators class

---
+++ Debt_Free_Game/DFGValidators.cpp
@@ -0,0 +1,13 @@
+//
+//  DFGValidators.cpp
+//  DEBT_SNOWBALL_GAME
+//
+//  Created by Lando Shepherd on 10/24/18.
+//  Copyright © 2018 Lando Shepherd. All rights reserved.
+//
+
+#include "DFGValidators.hpp"
+
+DFGValidators::DFGValidators(){};
+
+DFGValidators::~DFGValidators(){};
---
+++ Debt_Free_Game/DFGValidators.hpp
@@ -0,0 +1,22 @@
+//
+//  DFGValidators.hpp
+//  DEBT_SNOWBALL_GAME
+//
+//  Created by Lando Shepherd on 10/24/18.
+//  Copyright © 2018 Lando Shepherd. All rights reserved.
+//
+
+#ifndef DFGValidators_hpp
+#define DFGValidators_hpp
+
+#include <iostream>
+
+class DFGValidators
+{
+private:
+public:
+    DFGVavlidators();
+    ~DFGValidators();
+};
+
+#endif 
