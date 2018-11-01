0982569c9e800dfc6c10fca41a274c4b7edac303
Lando Shepherd <landoshepherd@landos-macbook-pro.local>
2018-10-31 23:32:36 -0500

    Initial Commit

---
+++ Debt_Free_Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Debt_Free_Game
+//
+//  Created by Lando Shepherd on 10/31/18.
+//  Copyright © 2018 Lando Shepherd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
