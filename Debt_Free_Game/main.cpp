e6746fd92945e1a759e679257475b54207fc11cd
Lando Shepherd <landoshepherd@landos-macbook-pro.local>
2018-10-31 23:36:13 -0500

    First commit directly from Xcode IDE

--- Debt_Free_Game/main.cpp
+++ Debt_Free_Game/main.cpp
@@ -1,15 +1,19 @@
 //
 //  main.cpp
-//  Debt_Free_Game
+//  DEBT_SNOWBALL_GAME
 //
-//  Created by Lando Shepherd on 10/31/18.
+//  Created by Lando Shepherd on 10/20/18.
 //  Copyright © 2018 Lando Shepherd. All rights reserved.
 //
 
 #include <iostream>
+#include "DFSGame.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    DFSGame *newGame;
+    newGame = new DFSGame;
+    
+    newGame->startGame();
+    delete newGame;
     return 0;
 }
